9eef8ee50c2a4352d2459e7996bdfd5bcac53ed9
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-12-03 17:42:09 +0300

    add comments to the admin py

--- stream/admin.py
+++ stream/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from .models import Post,Profile,Comment
 '''
     >>>filter_horizontal property allows ordering of many to many fields<<<
+    >>>since we don't have many to many relations in the models. We will not use filter_horizontal<<<
     class HyperAdmin(admin.ModelAdmin):
         filter_horizontal = ('',) 
 '''
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Profile)
-admin.site.register(Comment)
+admin.site.register(Post),
+admin.site.register(Profile),
+admin.site.register(Comment),
