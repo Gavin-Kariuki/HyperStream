4b3b204aeb79e008b6747b8f466421ce39d0efbf
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-12-03 17:39:52 +0300

    add models to the admin page

--- stream/admin.py
+++ stream/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post,Profile,Comment
+'''
+    >>>filter_horizontal property allows ordering of many to many fields<<<
+    class HyperAdmin(admin.ModelAdmin):
+        filter_horizontal = ('',) 
+'''
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Comment)
