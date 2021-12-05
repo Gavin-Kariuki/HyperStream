23f7b902f5a61c37f47d093a2c49625d5a0a54de
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-12-05 14:33:23 +0300

    add a home_page template

--- stream/urls.py
+++ stream/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home_page, name="home"),
     path("<int:id>/delete", views.delete_post, name = "delete_post"),
-    path("new_post/", views.new_post, name = "add_post"),
+    path("new_post/", views.new_post, name = "new_post"),
     path("<int:pk>/", views.post_detail, name = "post_detail"),
     path("<int:pk>", views.like, name = "likes"),
 ]
