1f232158d83034699199fcaef35cc4e961e3f1fc
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-12-05 14:23:47 +0300

    add urls to the app

--- stream/urls.py
+++ stream/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_page, name="home")
+    path("", views.home_page, name="home"),
+    path("<int:id>/delete", views.delete_post, name = "delete_post"),
+    path("new_post/", views.new_post, name = "add_post"),
+    path("<int:pk>/", views.post_detail, name = "post_detail"),
+    path("<int:pk>", views.like, name = "likes"),
 ]
